df461f5e51c27bb5985221dbe0fff69e5ab9ae84
Sarim Abbas <sarim.stays@gmail.com>
2023-10-19 00:45:58 -0700

    wip

--- javascript/packages/server/src/index.ts
+++ javascript/packages/server/src/index.ts
@@ -1,8 +1,17 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import { z } from "zod";
 
 const app = new Hono();
+
 app.get("/", (c) => c.text("Hello Hono!"));
+
+app.post("/test", zValidator("json", z.object({ name: z.string() })), (c) => {
+  console.log(c.body);
+  const data = c.req.valid("json");
+  return c.json({ name: data.name });
+});
 
 serve(app);
 
