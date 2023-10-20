892c0744321ac2f1f771ab177fe849b595f80c60
Sarim Abbas <sarim.stays@gmail.com>
2023-10-19 23:10:00 -0700

    wip

--- javascript/packages/server/src/index.ts
+++ javascript/packages/server/src/index.ts
@@ -2,16 +2,33 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
+import { $ } from "zx";
 
 const app = new Hono();
 
 app.get("/", (c) => c.text("Hello world!"));
 
-app.post("/test", zValidator("json", z.object({ name: z.string() })), (c) => {
-  console.log(c.body);
-  const data = c.req.valid("json");
-  return c.json({ name: data.name });
-});
+app.post(
+  "/musicgen",
+  zValidator(
+    "json",
+    z.object({
+      audioFilePath: z.string(),
+      startTime: z.number(),
+      endTime: z.number(),
+    })
+  ),
+  async (c) => {
+    console.log(c.body);
+    const data = c.req.valid("json");
+
+    // extract the region of the audio to a temp file
+    await $`ffmpeg -y -i ${data.audioFilePath} -ss ${data.startTime} -to ${data.endTime} tmp.mp3`;
+
+    // send it to music gen
+    return c.json({ success: true });
+  }
+);
 
 serve(app);
 
