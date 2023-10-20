05ea5f7fa813508283c67c85eb26e3d16109759b
Sarim Abbas <sarim.stays@gmail.com>
2023-10-20 02:37:19 -0700

    run model

--- javascript/packages/shared/src/lib/openapi/hooks.ts
+++ javascript/packages/shared/src/lib/openapi/hooks.ts
@@ -4,3 +4,7 @@ import { createApiClient } from "./generated";
 const apiClient = createApiClient("http://localhost:8000/", {});
 
 export const apiHooks = new ZodiosHooks("openAPIHooks", apiClient);
+
+// note: wrap your <App/> in a queryClient provider like so:
+// javascript/packages/client-music-gen/src/routes/root.tsx
+// learn more: https://github.com/ecyrbe/zodios-react
