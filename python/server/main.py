9878ce167e053884278775abc29f620d43adba2f
Sarim Abbas <sarim.stays@gmail.com>
2023-10-20 01:19:05 -0700

    wip

--- python/server/main.py
+++ python/server/main.py
@@ -1,4 +1,3 @@
-from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 import subprocess
@@ -9,11 +8,6 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
 
 
 class MusicGenRequestBody(BaseModel):
