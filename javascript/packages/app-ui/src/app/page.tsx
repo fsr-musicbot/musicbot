b67ff4f65b2df90251e0a621d42a836aa1121f48
Felicia <ffeliciachang@gmail.com>
2023-10-21 09:59:50 -0700

    wip

--- javascript/packages/app-ui/src/app/page.tsx
+++ javascript/packages/app-ui/src/app/page.tsx
@@ -1,17 +1,48 @@
 "use client";
-import { useDropzone } from "react-dropzone";
+import Dropzone from "react-dropzone";
 
 export default function Home() {
-  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+  const handleSubmitAudio = async (acceptedFiles: File[]) => {
+    console.log(acceptedFiles[0]);
 
-  console.log(acceptedFiles);
+    const reader = new FileReader();
+    reader.readAsDataURL(acceptedFiles[0]);
+
+    reader.onload = async function () {
+      const response = await fetch("/api", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(reader.result),
+      });
+      console.log(response);
+
+      if (response) {
+        console.log(await response.json());
+      }
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      // Here, you can make your API request with the base64String if needed
+      // For now, I'm just logging it for demonstration purposes.
+    };
+  };
 
   return (
     <section className="container">
-      <div {...getRootProps({ className: "dropzone" })}>
-        <input {...getInputProps()} />
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      </div>
+      <Dropzone onDrop={(acceptedFiles) => handleSubmitAudio(acceptedFiles)}>
+        {({ getRootProps, getInputProps }) => (
+          <section>
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              <p>Drag and drop some files here, or click to select files</p>
+            </div>
+          </section>
+        )}
+      </Dropzone>
     </section>
   );
 }
