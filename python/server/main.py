78d7e7dac9b7f33c7c65d861fab930af6920a652
Sarim Abbas <sarim.stays@gmail.com>
2023-10-21 01:35:40 -0700

    wip

--- python/server/main.py
+++ python/server/main.py
@@ -9,6 +9,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from .utils.main import mp3_to_base64_data_uri
 import requests
 
 
@@ -60,45 +61,52 @@ class MusicGenRequestBody(BaseModel):
 
 @app.post("/musicgen")
 def generate_music(body: MusicGenRequestBody):
+    input_audio_path = body.file_path
+
     # extract a slice of audio from the file path and save it as tmp.mp3
+    sliced_audio_path = os.path.join(dirname, "sliced.mp3")
     subprocess.run(
         [
             "ffmpeg",
             "-y",
             "-i",
-            body.file_path,
+            input_audio_path,
             "-ss",
             str(body.start_time),
             "-to",
             str(body.end_time),
-            os.path.join(dirname, "tmp.mp3"),
+            sliced_audio_path,
         ]
     )
 
+    sliced_audio_data_uri = mp3_to_base64_data_uri(sliced_audio_path)
+
     # send it to musicgen
-    with open(os.path.join(dirname, "tmp.mp3"), "rb") as infile:
-        output = replicate.run(
-            "meta/musicgen:7a76a8258b23fae65c5a22debb8841d1d7e816b75c2f24218cd2bd8573787906",
-            input={
-                # allows for conditioning on a melody
-                "model_version": "melody",
-                # prompt
-                "prompt": "a country guitar riff",
-                # audio file to generate music from
-                "input_audio": infile,
-                # number of seconds to generate
-                "duration": 8,
-                # if true, then it will continue the input audio
-                # if false, it will adopt the style of the input audio
-                "continuation": True,
-                # can be wav or mp3
-                "output_format": "mp3",
-            },
-        )
+    output = replicate.run(
+        "meta/musicgen:7a76a8258b23fae65c5a22debb8841d1d7e816b75c2f24218cd2bd8573787906",
+        input={
+            # allows for conditioning on a melody
+            "model_version": "melody",
+            # prompt
+            "prompt": "a country guitar riff",
+            # audio file to generate music from
+            "input_audio": sliced_audio_data_uri,
+            # number of seconds to generate
+            "duration": 8,
+            # if true, then it will continue the input audio
+            # if false, it will adopt the style of the input audio
+            "continuation": True,
+            # can be wav or mp3
+            "output_format": "mp3",
+        },
+    )
+
+    print(output)
 
     # get the output audio from url
+    generated_audio_path = os.path.join(dirname, "generated.mp3")
     response = requests.get(output)
-    with open(os.path.join(dirname, "out.mp3"), "wb") as outfile:
+    with open(generated_audio_path, "wb") as outfile:
         outfile.write(response.content)
 
     # Your code for generating music goes here
